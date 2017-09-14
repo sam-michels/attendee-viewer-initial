844c663dc00e0d09d019d42853f1408147e8ea6d
sam-michels <sam.michels@cgi.com>
2017-09-14 14:02:38 -0600

    oauth2

--- src/main/java/io/rscale/training/attendeeviewer/AttendeeViewerApplication.java
+++ src/main/java/io/rscale/training/attendeeviewer/AttendeeViewerApplication.java
@@ -5,16 +5,25 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @SpringBootApplication
 @EnableFeignClients
 @EnableDiscoveryClient
 @EnableCircuitBreaker
-public class AttendeeViewerApplication {
+@EnableOAuth2Sso
+public class AttendeeViewerApplication extends WebSecurityConfigurerAdapter {
 
 	public static void main(String[] args) {
 		SpringApplication.run(AttendeeViewerApplication.class, args);
 	}
 	
-
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/login").permitAll()
+			.anyRequest().authenticated();
+	}
 }
