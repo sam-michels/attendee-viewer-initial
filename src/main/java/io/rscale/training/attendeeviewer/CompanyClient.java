c224bb137df9493dd817691b79d98d6934675a3f
sam-michels <31221145+sam-michels@users.noreply.github.com>
2017-09-14 09:37:54 -0700

    Add files via upload

--- src/main/java/io/rscale/training/attendeeviewer/CompanyClient.java
+++ src/main/java/io/rscale/training/attendeeviewer/CompanyClient.java
@@ -1,7 +1,18 @@
 package io.rscale.training.attendeeviewer;
 
-public interface CompanyClient {
+import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.hateoas.PagedResources;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
-	Company getCompany(String id);
+@FeignClient(name = "company")
+public interface CompanyClient {
+	@RequestMapping(method = RequestMethod.GET, value = "/companies")
+    PagedResources<Company> getCompanies(@RequestParam("page") int page);
+
+    @RequestMapping(method = RequestMethod.GET, value = "/companies/{id}")
+	Company getCompany(@PathVariable("id") String id);
 	
 }
